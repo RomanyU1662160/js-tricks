b3b464954b3ae9c5bb4dbf4df77e14c9861aaf6f
Romany Sefen <35735454+RomanyU1662160@users.noreply.github.com>
2025-11-15 06:11:10 +0000

    add more examples

---
+++ Codility/Q27.ts
@@ -0,0 +1,35 @@
+const findFirstRepeatedChar = (input)=> {
+if(!input) return null 
+const seen = new Set()
+
+for (const char of input) {
+  if(seen.has(char)){
+    return char
+  }
+  seen.add(char)
+}
+return null
+}
+
+
+/// using map 
+
+const findFirstRepeatedChar2 = (input: string)=> {
+    if(!input) return null 
+
+    const counts = new Map<String, number>()
+  
+    // First loop: count characters
+    for(const char of input){
+      counts.set(char, (counts.get(char) || 0) +1)
+    }
+
+     // second loop, return the first char with count bigger than 1 
+    for(const char of input){
+      if(counts.get(char)! > 1) {
+        return char
+      }
+    }
+  // return null  
+    return null 
+}
